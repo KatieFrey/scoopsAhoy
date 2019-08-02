31cad77635daf5d9eec4a379ad0d1ec3dd1b16ea
Katharine Francis <katie-francis@hotmail.com>
2019-08-02 15:58:25 -0400

    Initial commit

---
+++ client/src/ChatBox.js
@@ -0,0 +1,107 @@
+import React from "react";
+import { Modal, Typography, Button, AppBar, Grid } from "@material-ui/core";
+import moduleStyles from "./ChatBox.module.css";
+//import { makeStyles } from "@material-ui/core/styles";
+
+// const useStyles = makeStyles(theme => ({
+//   nav: {
+//     position: "absolute",
+//     width: 400,
+//     backgroundColor: theme.palette.background.primary,
+//     border: "2px solid #000",
+//     boxShadow: theme.shadows[5],
+//     padding: theme.spacing(2, 4, 4),
+//     outline: "none"
+//   }
+// }));
+
+class ChatBox extends React.Component {
+  state = {
+    chat: "Hey nerd!",
+    open: false
+  };
+  closeHandler = () => {
+    this.setState({
+      open: false
+    });
+  };
+
+  handleOpen = () => {
+    this.setState({
+      open: true
+    });
+  };
+
+  render() {
+    //const classes = useStyles();
+    let { open, chat } = this.state;
+    console.log("open: ", open);
+    return (
+      <div>
+        <AppBar
+          style={{
+            backgroundColor: "theme.pallete.primary.main",
+            margin: "20px auto",
+            padding: "10px 50px"
+          }}
+        >
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+          >
+            <Typography style={{ color: "white" }} variant="h3">
+              Scoops Ahoy
+            </Typography>
+            <Button onClick={this.handleOpen}>
+              <Typography
+                variant="h4"
+                style={{
+                  color: "white"
+                }}
+              >
+                Chat with Steve
+              </Typography>
+            </Button>
+          </Grid>
+        </AppBar>
+
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={open}
+          onClose={this.closeHandler}
+        >
+          <div
+            style={{
+              top: "calc(50% - 20%)",
+              left: "calc(50% - 15%)",
+              position: "absolute",
+              width: 400,
+              height: 400,
+              backgroundColor: "black",
+              color: "white",
+              border: "2px solid #000",
+              boxShadow: "theme.shadows[5]",
+              padding: "theme.spacing(2, 4, 4)",
+              outline: "none"
+            }}
+          >
+            <p id="simple-modal-description">{chat}</p>
+            <input
+              className={moduleStyles.input}
+              width="80%"
+              type="text"
+              id="chat"
+              name="chat"
+              placeholder="What's you're question?"
+            />
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default ChatBox;
---
+++ client/src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import ChatBox from "./ChatBox";
+import * as serviceWorker from "./serviceWorker";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import pink from "@material-ui/core/colors/pink";
+import purple from "@material-ui/core/colors/purple";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: purple[100]
+    },
+    secondary: {
+      main: pink[100]
+    }
+  }
+});
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <ChatBox />
+  </MuiThemeProvider>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
